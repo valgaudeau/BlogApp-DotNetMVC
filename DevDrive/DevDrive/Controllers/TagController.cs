77c2ba6b333ed78d15ab47bd665ab20ae5deaed8
valgaudeau <valgaudeau@outlook.com>
2024-01-03 20:30:33 +0000

    created TagController, VM, Add & Index Views

---
+++ DevDrive/DevDrive/Controllers/TagController.cs
@@ -0,0 +1,48 @@
+﻿using DevDrive.Data;
+using DevDrive.Models.Domain;
+using DevDrive.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevDrive.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly BlogDbContext _dbContext;
+
+        public TagController(BlogDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        [HttpGet]
+        [ActionName("Index")]
+        public IActionResult Index()
+        {
+            var allTags = _dbContext.Tags.ToList();
+
+            return View(allTags);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagVM addTagVM)
+        {
+            var tag = new Tag
+            {
+                Name = addTagVM.Name,
+                DisplayName = addTagVM.DisplayName,
+            };
+
+            _dbContext.Tags.Add(tag);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ DevDrive/DevDrive/Models/ViewModels/AddTagVM.cs
@@ -0,0 +1,8 @@
+﻿namespace DevDrive.Models.ViewModels
+{
+    public class AddTagVM
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
